c1e0dc46e38313eb4fd1fe12010c47268ffdf8d1
Senthee1811 <esentheepan@gmail.com>
2024-07-25 20:45:41 +0530

    Home page initial DONE

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 function Home() {
+  const [category,setCategory] = useState("All")
   return (
     <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
       
     </div>
   )
