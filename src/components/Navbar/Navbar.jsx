222924314189779eec9f04c493e669ea919b4182
Senthee1811 <esentheepan@gmail.com>
2024-07-28 10:28:52 +0530

    Login Page Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import {assets} from "../../assets/assets"
 import { Link } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu,setMenu] = useState("home") 
   return (
     <div className='navbar'>
-       <img src={assets.logo} alt="" className='logo' />
+      <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
        <ul className='navbar-menu'>
         <Link to='/' onClick={()=> {setMenu("home")}} className={menu==='home' ? "active" : ""}>Home</Link>
         <a href='#explore-menu' onClick={()=> {setMenu("menu")}} className={menu === 'menu' ? "active" : ""}>Menu</a>
@@ -18,12 +18,12 @@ const Navbar = () => {
        <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
+           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> 
             <div className="dot">
                
             </div>
         </div>
-        <button>SignIn</button>
+        <button onClick={()=>{setShowLogin(true)}}>SignIn</button>
        </div>
     </div>
   )
