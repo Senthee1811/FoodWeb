9a39355408245833ffd85548ab54bf61c294a46d
Senthee1811 <esentheepan@gmail.com>
2024-07-30 20:22:48 +0530

    Backend Startd

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"; 
+import cors from "cors"
+
+//app config
+const app = express();
+const PORT = 4000; 
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
