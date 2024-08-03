609f37607505dfcbb3ef047eb1b9cf8fc9e94a24
Senthee1811 <esentheepan@gmail.com>
2024-08-03 16:02:24 +0530

    Admin Add page and List page Done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,12 +5,16 @@ import { Route, Routes } from 'react-router-dom'
 import List from './pages/List/List'
 import Order from './pages/Orders/Order'
 import Add from './pages/Add/Add'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   
 
   return (
     <div>
+      <ToastContainer/>
       <NavBar/>
       <hr />
       <div className="app-content">
